03a7a377cb33407a21475a4724a68f68da50e856
Brendan Hack <github@bendys.com>
2020-09-13 11:26:32 +1000

    report performance in megapixels/bytes

--- src/bin/kperf/kperf.cpp
+++ src/bin/kperf/kperf.cpp
@@ -5,6 +5,15 @@
 #include <iostream>
 #include <vector>
 
+void report(const libio::Frame& frame, std::uint32_t frame_count, const std::chrono::duration<float>& duration)
+{
+    std::cout << frame_count << "x" << frame.width << "x" << frame.height << " took " << duration.count() << "s" << std::endl;
+    std::cout << "    " << static_cast<float>(frame_count) / duration.count() << " f/sec" << std::endl;
+    std::cout << "    " << (frame_count * frame.width * frame.height / 1000000.0f) / duration.count() << " megapixels/sec" << std::endl;
+    std::cout << "    " << (frame_count * frame.width * frame.height * frame.comp_size * frame.n_comp / 1000000.0f) / duration.count() << " megabytes/sec" << std::endl;
+    std::cout << std::endl;
+}
+
 int main(int argc, char** argv)
 {
     libio::Frame frame_in(1920, 1080, 1, 4);
@@ -29,18 +38,11 @@ int main(int argc, char** argv)
             k.process(frame_in.data.get(), frame_out.data.get());
             duration += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start);
         }
-        std::cout << frame_count << "x" << frame_in.width << "x" << frame_in.height << " took " << duration.count() << "s" << std::endl;
-        std::cout << "    " << static_cast<float>(frame_count) / duration.count() << " f/sec" << std::endl;
-        std::cout << "    " << (frame_count * frame_in.width * frame_in.height) / duration.count() << " pixels/sec" << std::endl;
-        std::cout << "    " << (frame_count * frame_in.width * frame_in.height * frame_in.comp_size * frame_in.n_comp) / duration.count() << " bytes/sec" << std::endl;
-        std::cout << std::endl;
+        report(frame_in, frame_count, duration);
+
         total += duration;
         total_frames += frame_count;
     }
-    std::cout << total_frames << "x" << frame_in.width << "x" << frame_in.height << " took " << total.count() << "s" << std::endl;
-    std::cout << "    " << static_cast<float>(total_frames) / total.count() << " f/sec" << std::endl;
-    std::cout << "    " << (total_frames * frame_in.width * frame_in.height) / total.count() << " pixels/sec" << std::endl;
-    std::cout << "    " << (total_frames * frame_in.width * frame_in.height * frame_in.comp_size * frame_in.n_comp) / total.count() << " bytes/sec" << std::endl;
-    std::cout << std::endl;
+    report(frame_in, total_frames, total);
     return 0;
 }
